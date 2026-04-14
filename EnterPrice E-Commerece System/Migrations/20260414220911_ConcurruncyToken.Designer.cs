57d3a746c7c81c68901c62af6fcb34ad52191941
Ahmed <Ahmed3679117@gmail.com>
2026-04-15 00:13:15 +0200

    added Migration

--- EnterPrice E-Commerece System/Migrations/20260414220911_ConcurruncyToken.Designer.cs
+++ EnterPrice E-Commerece System/Migrations/20260414220911_ConcurruncyToken.Designer.cs
@@ -4,6 +4,7 @@ using EnterPrice_E_Commerece_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EnterPrice_E_Commerece_System.Migrations
 {
     [DbContext(typeof(EnterPriseContext))]
-    partial class EnterPriseContextModelSnapshot : ModelSnapshot
+    [Migration("20260414220911_ConcurruncyToken")]
+    partial class ConcurruncyToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EnterPrice E-Commerece System/Migrations/20260414220911_ConcurruncyToken.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnterPrice_E_Commerece_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConcurruncyToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Inventories",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Products_Quantity",
+                table: "Inventories",
+                sql: "[QuantityAvailable] >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Products_Quantity",
+                table: "Inventories");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Inventories");
+        }
+    }
+}
